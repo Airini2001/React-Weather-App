bea9bd2bcdaaad461292cfcb8fae46ed42e8df2c
Irina <111460351+Airini2001@users.noreply.github.com>
2022-10-04 23:04:34 +0300

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,16 +1,41 @@
-/** @format */
+import React, { useState } from "react";
 
-import React from "react";
-
-export default function WeatherTemperature(props) {
+export default function WeatherTemp(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahren(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
     return (
-      <div className='WeatherTemperature'>
-        <span className='temperature'>{Math.round(props.celsius)}</span>
-        <span className='unit'>
-                     °C
-                
+      <div className="WeatherTemp">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{""}{" "}
+          <a href="/" onClick={showFahren}>
+            {" "}
+            F°
+          </a>
         </span>
       </div>
     );
+  } else {
+    let fahren = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemp">
+        <span className="temperature">{Math.round(fahren)}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            {" "}
+            °C{" "}
+          </a>{" "}
+          {""} | F°
+        </span>
+      </div>
+    );
+  }
 }
-
